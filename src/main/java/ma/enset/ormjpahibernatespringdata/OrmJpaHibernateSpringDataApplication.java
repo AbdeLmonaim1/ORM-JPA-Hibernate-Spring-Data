c7dc365f56f248351767ab5412c997221b8fbda3
AbdeLmonaim1 <abdelmonaimahdoud3@gmail.com>
2025-03-12 04:15:34 +0000

    H2 database configuration and test patienRepository

--- src/main/java/ma/enset/ormjpahibernatespringdata/OrmJpaHibernateSpringDataApplication.java
+++ src/main/java/ma/enset/ormjpahibernatespringdata/OrmJpaHibernateSpringDataApplication.java
@@ -1,7 +1,13 @@
 package ma.enset.ormjpahibernatespringdata;
 
+import ma.enset.ormjpahibernatespringdata.dao.PatientRepository;
+import ma.enset.ormjpahibernatespringdata.entities.Patient;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class OrmJpaHibernateSpringDataApplication {
@@ -9,5 +15,28 @@ public class OrmJpaHibernateSpringDataApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrmJpaHibernateSpringDataApplication.class, args);
     }
+//    @Bean
+    CommandLineRunner commandLineRunner(PatientRepository patientRepository) {
+        return args -> {
+            patientRepository.save(Patient.builder()
+                            .name("Monaim")
+                            .dateOfBirth(new Date())
+                            .sick(true)
+                            .score(26)
+                    .build());
+            patientRepository.save(Patient.builder()
+                    .name("Amine")
+                    .dateOfBirth(new Date())
+                    .sick(true)
+                    .score(11)
+                    .build());
+            patientRepository.save(Patient.builder()
+                    .name("Mouad")
+                    .dateOfBirth(new Date())
+                    .sick(true)
+                    .score(17)
+                    .build());
 
+        };
+    }
 }
--- src/main/java/ma/enset/ormjpahibernatespringdata/entities/Patient.java
+++ src/main/java/ma/enset/ormjpahibernatespringdata/entities/Patient.java
@@ -1,10 +1,8 @@
 package ma.enset.ormjpahibernatespringdata.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 @Entity
@@ -13,6 +11,8 @@ public class Patient {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @Temporal(TemporalType.DATE)
+//    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dateOfBirth;
     private boolean sick;
     private int score;
