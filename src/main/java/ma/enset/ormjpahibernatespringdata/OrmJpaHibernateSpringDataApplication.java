a234a1a599105ec99543ec5e946dfa752e2db19f
AbdeLmonaim1 <abdelmonaimahdoud3@gmail.com>
2025-03-12 15:43:42 +0000

    Test all features

--- src/main/java/ma/enset/ormjpahibernatespringdata/OrmJpaHibernateSpringDataApplication.java
+++ src/main/java/ma/enset/ormjpahibernatespringdata/OrmJpaHibernateSpringDataApplication.java
@@ -12,10 +12,14 @@ import java.util.Date;
 @SpringBootApplication
 public class OrmJpaHibernateSpringDataApplication {
 
+
+
+
+
     public static void main(String[] args) {
         SpringApplication.run(OrmJpaHibernateSpringDataApplication.class, args);
     }
-//    @Bean
+    @Bean
     CommandLineRunner commandLineRunner(PatientRepository patientRepository) {
         return args -> {
             patientRepository.save(Patient.builder()
@@ -38,5 +42,25 @@ public class OrmJpaHibernateSpringDataApplication {
                     .build());
 
         };
+    }
+    @Bean
+    CommandLineRunner commandLineRunnerInit(PatientRepository patientRepository) {
+        return args -> {
+            System.out.println("List of all patients");
+            patientRepository.findAll().forEach(System.out::println);
+            System.out.println("Patient by ID: " + patientRepository.findById(2L).get().toString());
+            System.out.println("Patient by name: " + patientRepository.findByName("Monaim").toString());
+            System.out.println("Update a patient by ID: ");
+            Patient patient = patientRepository.findById(2L).get();
+            patient.setScore(100);
+            patient.setSick(false);
+            patientRepository.save(patient);
+            System.out.println("Patient by ID: " + patientRepository.findById(2L).get().toString());
+            System.out.println("Delete a patient by ID: ");
+            patientRepository.deleteById(3L);
+            System.out.println("List of all patients after deletion: ");
+            patientRepository.findAll().forEach(System.out::println);
+        };
+
     }
 }
--- src/main/java/ma/enset/ormjpahibernatespringdata/dao/PatientRepository.java
+++ src/main/java/ma/enset/ormjpahibernatespringdata/dao/PatientRepository.java
@@ -3,5 +3,8 @@ package ma.enset.ormjpahibernatespringdata.dao;
 import ma.enset.ormjpahibernatespringdata.entities.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+    List<Patient> findByName(String name);
 }
